583887029f29b92eff228d18d1ccc8f7655990f8
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-02-07 08:52:28 -0500

    Created Intellij

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,33 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        /* BMI = km/m*m
+           m = inch *(0.0254/1)
+           inch = m*(1/0.0254)
+           kg = inch *()
+           inch = kg *()
+        */
+        String heightInInches;
+        String heightInFeet;
+        Scanner keyboard;
+
+        keyboard = new Scanner(System.in);
+        System.out.println("What is your height (only feet)?");
+        heightInFeet = keyboard.nextLine();
+        System.out.println("What is your height (only inches)?");
+        heightInInches = keyboard.nextLine();
+
+
+        int firstNumber, secondNumber, total;
+        firstNumber = 5;
+        secondNumber = 6;
+        total = firstNumber+secondNumber;
+    }
+
+
+}
